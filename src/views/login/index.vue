0d0875891af88b4f805781f5cba309b16641ba75
Ficus <ficusf@qq.com>
2022-10-08 23:36:14 +0800

    request api.

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,18 +3,40 @@
 </template>
 
 <script>
+import { login } from '@/api/user'
+
 export default {
   name: 'LoginIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    async onsubmit () {
+      const user = this.user
+
+      try {
+        const res = await login(user)
+        console.log('登录成功', res)
+      } catch (err) {
+        if (err.response.status === 400) {
+          console.log('手机号或验证码错误')
+        } else {
+          console.log('登录失败', err)
+        }
+      }
+    }
+  }
 }
 </script>
 
